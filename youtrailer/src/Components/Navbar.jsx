ec915f59a1ac370f7986dc5620b625bba8751d46
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-12-10 19:38:46 +1030

    firebase setup

--- youtrailer/src/Components/Navbar.jsx
+++ youtrailer/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className=" flex items-center justify-between p-4 z-[100] w-full absolute">
-      <h1 className=" text-red-600 text-4xl font-bold cursor-pointer">
+      <h1 className=" text-red-600 text-4xl font-extrabold cursor-pointer">
         YOUTRAILER
       </h1>
       <div>
--- youtrailer/src/Pages/Home.jsx
+++ youtrailer/src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Main from "../Components/Main";
+import Row from "../Components/Row";
+import requests from "../request";
 
 const Home = () => {
   return (
     <div>
       <Main />
+      <Row rowId="1" title="Up Coming" fetchURL={requests.requestUpcoming} />
+      <Row rowId="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowId="3" title="Trending" fetchURL={requests.requestTopRated} />
+      <Row rowId="4" title="Top Rated" fetchURL={requests.requestUpcoming} />
+      <Row rowId="5" title="Horror" fetchURL={requests.requestHorror} />
     </div>
   );
 };
